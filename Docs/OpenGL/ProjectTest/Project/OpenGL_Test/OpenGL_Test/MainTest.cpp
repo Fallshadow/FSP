57d146790096c9dc1b6feae213e593f1e10f86f8
sunshuchao <www.sunshuchao@outlook.com>
2022-11-17 09:41:28 +0800

    【更新】-【OpenGL纹理】

--- Docs/OpenGL/ProjectTest/Project/OpenGL_Test/OpenGL_Test/MainTest.cpp
+++ Docs/OpenGL/ProjectTest/Project/OpenGL_Test/OpenGL_Test/MainTest.cpp
@@ -8,6 +8,8 @@
 void processInput(GLFWwindow* window);
 GLFWwindow* initWindowEnvironment(float width, float height);
 
+float mixValue = 0.2f;
+
 int main() 
 {
 	GLFWwindow* window = initWindowEnvironment(800, 600);
@@ -15,10 +17,10 @@ int main()
 
 	float vertices[] =
 	{
-		0.5f, 0.5f, 0.0f,   // 右上角
-		0.5f, -0.5f, 0.0f,  // 右下角
-		-0.5f, -0.5f, 0.0f, // 左下角
-		-0.5f, 0.5f, 0.0f   // 左上角
+		0.5f, 0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	1.0f, 1.0f, // 右上角 0.55f, 0.55f,
+		0.5f, -0.5f, 0.0f,    0.0f, 1.0f, 0.0f,	1.0f, 0.0f, // 右下角 0.55f, 0.45f,
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,	0.0f, 0.0f, // 左下角 0.45f, 0.45f,
+		-0.5f, 0.5f, 0.0f,    0.0f, 0.0f, 0.0f,	0.0f, 1.0f, // 左上角 0.45f, 0.55f 
 	};
 
 	float vertices2[] =
@@ -34,6 +36,11 @@ int main()
 		// 此例的索引(0,1,2,3)就是顶点数组vertices的下标，
 		// 这样可以由下标代表顶点组合成矩形
 
+		0, 1, 3, // 第一个三角形
+		1, 2, 3  // 第二个三角形
+	};
+
+	unsigned int indices2[] = {
 		0, 1, 3, // 第一个三角形
 		1, 2, 3  // 第二个三角形
 	};
@@ -55,16 +62,23 @@ int main()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[0]);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+	glBindVertexArray(VAOs[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[1]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices2), indices2, GL_STATIC_DRAW);
 
-	unsigned int tex_test1;
-	glGenTextures(1, &tex_test1);
+	unsigned int tex_test[2];
+	glGenTextures(2, tex_test);
 
-	glBindTexture(GL_TEXTURE_2D, tex_test1);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+	glBindTexture(GL_TEXTURE_2D, tex_test[0]);
 	int weight, height, nrChannels;
 	unsigned char* data = stbi_load("wall.jpg", &weight, &height, &nrChannels, 0);
 	if(data)
@@ -78,42 +92,57 @@ int main()
 	}
 	stbi_image_free(data);
 
-	Shader MYShader("Vertex_0P1C2T_OutColorAndTexture.Shader", "Fragment_InCT.Shader");
-	Shader MYShader_UniformColor("Vertex_0P1C2T_OutColorAndTexture.Shader", "Fragment_InCT_MixTime.Shader");
-	Shader MYShader_UniformColor_NoT("Vertex_0P1C2T_OutColorAndTexture.Shader", "Fragment_UniformColor.Shader");
+	glBindTexture(GL_TEXTURE_2D, tex_test[1]);
+	stbi_set_flip_vertically_on_load(true);
+	data = stbi_load("awesomeface.png", &weight, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, weight, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
 
+	Shader MYShader_MixT("Vertex_0P1C2T_OutColorAndTexture.Shader", "Fragment_InCT_MixTexture.Shader");
+
+	MYShader_MixT.Use();
+	glUniform1i(glGetUniformLocation(MYShader_MixT.ID, "UniformTexture2D_1"), 0);
+	MYShader_MixT.SetInt("UniformTexture2D_2", 1);
+	
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
-
+		MYShader_MixT.SetFloat("UniformMix", mixValue);
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		// 激活程序
-		MYShader.Use();
-		glBindTexture(GL_TEXTURE_2D, tex_test1);
-		glBindVertexArray(VAOs[0]);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, tex_test[0]);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glActiveTexture(GL_TEXTURE1);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glBindTexture(GL_TEXTURE_2D, tex_test[1]);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+		float borderColor[] = { 1.0f, 1.0f, 0.0f, 1.0f };
+		glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-		float timeValue = glfwGetTime();
-		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-
-		MYShader_UniformColor_NoT.Use();
-		int vertexColorLocation_noT = glGetUniformLocation(MYShader_UniformColor_NoT.ID, "UniformInColor");
-		glUniform4f(vertexColorLocation_noT, 1 - greenValue, greenValue, 0.5f - greenValue, 1.0f);
+		MYShader_MixT.Use();
+		glBindVertexArray(VAOs[1]);
+		
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-		MYShader_UniformColor.Use();
-		
-		int vertexColorLocation = glGetUniformLocation(MYShader_UniformColor.ID, "UniformInColor");
-		glUniform4f(vertexColorLocation, 1 - greenValue, greenValue, 0.5f - greenValue, 1.0f);
-		glDrawArrays(GL_TRIANGLES, 1, 3);
-		
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-
-	MYShader.Delete();
 
 	glfwTerminate();
 	return 0;
@@ -123,6 +152,19 @@ void processInput(GLFWwindow *window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+	{
+		mixValue += 0.001f; // change this value accordingly (might be too slow or too fast based on system hardware)
+		if (mixValue >= 1.0f)
+			mixValue = 1.0f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+	{
+		mixValue -= 0.001f; // change this value accordingly (might be too slow or too fast based on system hardware)
+		if (mixValue <= 0.0f)
+			mixValue = 0.0f;
+	}
 }
 
 GLFWwindow* initWindowEnvironment(float width, float height)
